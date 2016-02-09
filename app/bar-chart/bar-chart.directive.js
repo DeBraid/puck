28a8b346f5a044f21b79b8744d81913ca484aee8
DeBraid <de.braid@gmail.com>
2016-02-09 12:29:11 -0500

    creating charting_data array of objects to graph

--- app/bar-chart/bar-chart.directive.js
+++ app/bar-chart/bar-chart.directive.js
@@ -21,17 +21,36 @@ function BarChartDirective (getData) {
 function BarChartLink (
     scope, elem, attrs
 ) {
+    console.log('scope.data', scope.data);
+    var data = scope.data;
     scope.title = 'Bar Chart for Teams: Foo';
     scope.metric = 'CFPct';
     scope.submit = function () {
-        console.log('drawNewChart metric', scope.metric);
+        // console.log('submit data', data[0]);
     }
 
-
     scope.$watch('metric', function(newVal, oldVal) {
-        // console.log('metric watcher: old' oldVal, 'new', newVal);
-        console.log('metric', newVal, oldVal);
+        var charting_data = [];
+        var data = scope.data;
+        if (!data.length) {
+            console.log('no data.length ');
+            return;  
+        }
+        var metrics = Object.keys(data[0]);
+        console.log('metrics', metrics);
+        
+        if ( metrics.indexOf(newVal) > -1 ) {
+            console.log('matching!', newVal );
+            data.map(function (team) {
+
+                charting_data.push({
+                    metric : newVal,
+                    value: team[newVal], 
+                    team : team.teamname
+                });
+            })
+        }
+        console.log('charting_data', charting_data);
     });
-    console.log('scope', scope.data);
     // console.log('$attr.data', $attr , $attr.data);
 }
