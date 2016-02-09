f6b0d8729e655503084e7af899f8ba0160eb09cd
DeBraid <de.braid@gmail.com>
2016-02-09 15:17:20 -0500

    bars will render, needs work

--- app/bar-chart/bar-chart.directive.js
+++ app/bar-chart/bar-chart.directive.js
@@ -22,41 +22,57 @@ function BarChartLink (
     scope, ele, attrs
 ) {
     console.log('scope.data', scope.data);
-    var data = scope.data;
+
+    scope.charting_data = [];
     scope.title = 'Bar Chart for Teams: Foo';
     scope.metric = 'CFPct';
-    scope.submit = function () {
-        // console.log('submit data', data[0]);
-    }
+    // scope.submit = function () {
+    //     // console.log('submit data', data[0]);
+    // }
 
     scope.$watch('metric', function(newVal, oldVal) {
-        var charting_data = [];
+        
+        console.log('newVal', newVal);
+        
         var data = scope.data;
-        if (!data.length) {
-            console.log('no data.length ');
-            return;  
-        }
         var metrics = Object.keys(data[0]);
+        if (!metrics) {return;}
         console.log('metrics', metrics);
         
         if ( metrics.indexOf(newVal) > -1 ) {
-            console.log('matching!', newVal );
+            
             data.map(function (team) {
-
-                charting_data.push({
+                scope.charting_data.push({
                     metric : newVal,
                     value: team[newVal], 
                     team : team.teamname
                 });
             });
-            console.log('charting_data', charting_data);
-            drawChart(charting_data);
+            // drawChart(scope.charting_data);
+            console.log('scope.charting_data', scope.charting_data);
+            scope.render(scope.charting_data);
         }
 
-        function drawChart (data) {
+        // function drawChart (data) {
             // console.log('data in drawChart', data);
             
             // console.log('svg', svg);
+            
+            
+            // window.onresize = function() {
+            //     scope.$apply();
+            // };
+            // scope.$watch(function() {
+            //     return angular.element(window)[0].innerWidth;
+            // }, function() {
+            //     scope.render(scope.data);
+            // });
+
+            // scope.$watch('data', function(newData) {
+            //     scope.render(newData);
+            // }, true);
+
+        scope.render = function(render_data) {
             var renderTimeout;
             var margin = parseInt(attrs.margin) || 20,
                 barHeight = parseInt(attrs.barHeight) || 20,
@@ -64,56 +80,57 @@ function BarChartLink (
             
             var svg = d3.select('#bar-chart')
                 .append('svg').style('width', '100%');
-            console.log('svg', svg);
-            
-            window.onresize = function() {
-                scope.$apply();
-            };
-            scope.$watch(function() {
-                return angular.element(window)[0].innerWidth;
-            }, function() {
-                scope.render(scope.data);
-            });
+                console.log('svg', svg);
 
-            scope.$watch('data', function(newData) {
-                scope.render(newData);
-            }, true);
+            // svg.selectAll('*').remove();
+            if (!render_data) return;
+            if (renderTimeout) clearTimeout(renderTimeout);
+          
+            renderTimeout = setTimeout(function() {
+                console.log('running setTimeout');
+                // console.log('scope.data', scope.data);
+                var width = 500,
+                    height = render_data.length * (barHeight + barPadding),
+                    color = d3.scale.category20(),
+                    min = d3.min(render_data, function(d) { return d.value; }),
+                    max = d3.max(render_data, function(d) { return d.value; });
+                
+                var xScale = d3.scale.linear()
+                        .domain([min,max])
+                        .range([0, width]);
 
-            scope.render = function(data) {
-                svg.selectAll('*').remove();
-                if (!data) return;
-                if (renderTimeout) clearTimeout(renderTimeout);
-                    renderTimeout = setTimeout(function() {
-                        console.log('scope.data', scope.data);
-                        var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-                            height = scope.data.length * (barHeight + barPadding),
-                            color = d3.scale.category20(),
-                            xScale = d3.scale.linear().domain([0, d3.max(data, function(d) {
-                                return d.value;
-                            })]).range([0, width]);
-                        svg.attr('height', height);
-                        svg.selectAll('rect').data(data).enter().append('rect').on('click', function(d, i) {
-                            return scope.onClick({
-                                item: d
-                            });
-                        }).attr('height', barHeight).attr('width', 140).attr('x', Math.round(margin / 2)).attr('y', function(d, i) {
-                            return i * (barHeight + barPadding);
-                        }).attr('fill', function(d) {
-                            return color(d.value);
-                        }).transition().duration(1000).attr('width', function(d) {
-                            return xScale(d.value);
-                        });
-                        svg.selectAll('text').data(data).enter().append('text').attr('fill', '#fff').attr('y', function(d, i) {
-                            return i * (barHeight + barPadding) + 15;
-                        }).attr('x', 15).text(function(d) {
-                            return d.team + " (scored: " + d.value + ")";
-                        });
-                    }, 200);
-            };
+                svg.attr('height', height);
+                svg.selectAll('rect')
+                .data(render_data).enter()
+                .append('rect').on('click', function(d, i) {
+                    return scope.onClick({
+                        item: d
+                    });
+                })
+                .attr('height', barHeight)
+                .attr('width', 140)
+                .attr('x', Math.round(margin / 2))
+                .attr('y', function(d, i) {
+                    return i * (barHeight + barPadding);
+                })
+                .attr('fill', function(d) {
+                    return color(d.value);
+                })
+                .transition().duration(1000).attr('width', function(d) {
+                    return xScale(d.value);
+                });
+                
+                svg.selectAll('text')
+                .data(data).enter().append('text')
+                .attr('fill', '#fff').attr('y', function(d, i) {
+                    return i * (barHeight + barPadding) + 15;
+                })
+                .attr('x', 15).text(function(d) {
+                    return d.team + " (scored: " + d.value + ")";
+                });
 
-        }
-
-
+            }, 200);
+        };
     });
     // console.log('$attr.data', $attr , $attr.data);
 }
