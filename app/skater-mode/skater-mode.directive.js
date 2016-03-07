3dd6c408801b325938115842458d0d22af4f63a9
DeBraid <de.braid@gmail.com>
2016-03-07 11:00:59 -0500

    ghetto box and whisker loading

--- app/skater-mode/skater-mode.directive.js
+++ app/skater-mode/skater-mode.directive.js
@@ -33,11 +33,11 @@ function SkaterModeController($scope, skaterModeServices) {
         var logo_path = setTeamImage($scope.skater);
         angular.extend( $scope.skater , logo_path );
         
-        var metric = 'GF60';
-        var quartiles = skaterModeServices.quartiles($scope.payload, metric);
-        var extent = skaterModeServices.extent($scope.payload, metric);
-        console.log('SkaterMode extent', extent);
-        console.log('SkaterMode quartiles', quartiles);
+        // var metric = 'GF60';
+        // var quartiles = skaterModeServices.quartiles($scope.payload, metric);
+        // var extent = skaterModeServices.extent($scope.payload, metric);
+        // console.log('SkaterMode extent', extent);
+        // console.log('SkaterMode quartiles', quartiles);
 	}
 	
 	function setTeamImage(skater) {
@@ -50,6 +50,80 @@ function SkaterModeController($scope, skaterModeServices) {
 function SkaterModeLink (
     scope, ele, attrs
 ) {
+
+    var margin = {top: 10, right: 50, bottom: 20, left: 50},
+    width = 120 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var min = Infinity,
+        max = -Infinity;
+
+    // var chart = skaterModeServices.box()
+    var chart = d3.box()
+        .whiskers(iqr(1.5))
+        .width(width)
+        .height(height);
+
+    d3.csv("app/skater-mode/morley.csv", function(error, csv) {
+      if (error) throw error;
+
+      var data = [];
+
+      csv.forEach(function(x) {
+        var e = Math.floor(x.Expt - 1),
+            r = Math.floor(x.Run - 1),
+            s = Math.floor(x.Speed),
+            d = data[e];
+        if (!d) d = data[e] = [s];
+        else d.push(s);
+        if (s > max) max = s;
+        if (s < min) min = s;
+      });
+
+      chart.domain([min, max]);
+
+      var svg = d3.select("#box-and-whisker-container")
+        .selectAll("svg")
+          .data(data)
+        .enter().append("svg")
+          .attr("class", "box")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.bottom + margin.top)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+          .call(chart);
+
+      setInterval(function() {
+        svg.datum(randomize).call(chart.duration(1000));
+      }, 2000);
+    });
+
+    function randomize(d) {
+      if (!d.randomizer) d.randomizer = randomizer(d);
+      return d.map(d.randomizer);
+    }
+
+    function randomizer(d) {
+      var k = d3.max(d) * .02;
+      return function(d) {
+        return Math.max(min, Math.min(max, d + k * (Math.random() - .5)));
+      };
+    }
+
+    // Returns a function to compute the interquartile range.
+    function iqr(k) {
+      return function(d, i) {
+        var q1 = d.quartiles[0],
+            q3 = d.quartiles[2],
+            iqr = (q3 - q1) * k,
+            i = -1,
+            j = d.length;
+        while (d[++i] < q1 - iqr);
+        while (d[--j] > q3 + iqr);
+        return [i, j];
+      };
+    }
+
     // var left_margin_from_foo;
     // scope.widerLeftColumn = false;
     // scope.charting_data = [];
--- app/skater-mode/skater-mode.services.js
+++ app/skater-mode/skater-mode.services.js
@@ -1,41 +1,40 @@
 
 angular
 	.module('puckalyticsMainApp.skaterModeServices', [])
-	.service('skaterModeServices', skaterModeServices );
+	.service('skaterModeServices', skaterModeServices);
 
-function skaterModeServices () {
-	return {
-		quartiles : calcQuartiles,
-		extent : calcExtent,
-		sortByMetric : sortByMetric
-	}
-	
-	// return array of quartiles 
-	function calcQuartiles(payload, metric) {
-		var sorted_arr = sortByMetric(payload, metric);
-		
-		return [
-			d3.quantile(sorted_arr, .25).toFixed(3),
-			d3.quantile(sorted_arr, .5).toFixed(3),
-			d3.quantile(sorted_arr, .75).toFixed(3)
-		];
-	}
+function skaterModeServices() {
+    return {
+        quartiles: calcQuartiles,
+        extent: calcExtent,
+        sortByMetric: sortByMetric,
+    }
+    // return array of quartiles 
+    function calcQuartiles(payload, metric) {
+        var sorted_arr = sortByMetric(payload, metric);
+        return [
+            d3.quantile(sorted_arr, .25).toFixed(3),
+            d3.quantile(sorted_arr, .5).toFixed(3),
+            d3.quantile(sorted_arr, .75).toFixed(3)
+        ];
+    }
 
-	function calcExtent(data, metric) {
-		var arr = sortByMetric(data, metric);
-		console.log('arr in extent');
-		return d3.extent(arr, function(d) { return d; });
-	}
-
-	// given array of objects, 
-	// return an a sorted flat array for given metric
-	function sortByMetric(data, metric) {
-		var arr = [];
-		d3.nest()
-			.key(function(d) { return d[metric]; })
-			.sortKeys(d3.descending)
-			.key(function(d) { return arr.push(d[metric]); })
-			.entries(data);
-		return arr;				
-	}
+    function calcExtent(data, metric) {
+        var arr = sortByMetric(data, metric);
+        console.log('arr in extent');
+        return d3.extent(arr, function(d) {
+            return d;
+        });
+    }
+    // given array of objects, 
+    // return an a sorted flat array for given metric
+    function sortByMetric(data, metric) {
+        var arr = [];
+        d3.nest().key(function(d) {
+            return d[metric];
+        }).sortKeys(d3.descending).key(function(d) {
+            return arr.push(d[metric]);
+        }).entries(data);
+        return arr;
+    }
 }
