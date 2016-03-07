c3a4b775cecb04c84bee5025e315e2db37c49892
DeBraid <de.braid@gmail.com>
2016-03-07 12:58:27 -0500

    working: will draw two metrics when provided array

--- app/skater-mode/skater-mode.directive.js
+++ app/skater-mode/skater-mode.directive.js
@@ -19,28 +19,22 @@ function SkaterModeDirective (skaterModeServices) {
         controller: SkaterModeController,
     }
 }
-/*
-	box and whisker examples: 
-	http://bl.ocks.org/jensgrubert/7789216
-*/
 
 function SkaterModeController($scope, skaterModeServices) {
     $scope.$watch( 'data' , init );
-
+    $scope.$watch( 'box_plot_metric' , function (val) {
+        console.log('val from box_plot_metric', val);
+        var metric = val;
+        $scope.charting_data = skaterModeServices.createRenderData($scope.payload, metric);
+    });
     function init(skater) {
         if (!skater || !skater.length) { return; }
         $scope.skater = skater[0];      
         var logo_path = setTeamImage($scope.skater);
         angular.extend( $scope.skater , logo_path );
-        
-
-        var metric = 'GF60';
-        // var quartiles = skaterModeServices.quartiles($scope.payload, metric);
-        // var extent = skaterModeServices.extent($scope.payload, metric);
-        var createRenderData = skaterModeServices.createRenderData($scope.payload, metric);
-        // console.log('SkaterMode extent', extent);
-        // console.log('SkaterMode quartiles', quartiles);
-        $scope.charting_data = createRenderData;
+        // var metrics = 'GA';        
+        var metrics = ['CF60', 'CA60'];        
+        $scope.charting_data = skaterModeServices.createRenderData($scope.payload, metrics);
 	}
 	
 	function setTeamImage(skater) {
@@ -51,15 +45,16 @@ function SkaterModeController($scope, skaterModeServices) {
 }
 
 function SkaterModeLink (
-    scope, ele, attrs
+    scope, ele, attrs, skaterModeServices
 ) {
     scope.$watch( 'charting_data' , function (val) {
+        console.log('watcher for charting_data GO!');
         render(val);
     });
 
     var margin = {top: 10, right: 50, bottom: 20, left: 50},
     width = 120 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 400 - margin.top - margin.bottom;
 
     var min = Infinity,
         max = -Infinity;
@@ -71,50 +66,36 @@ function SkaterModeLink (
         .height(height);
 
     // d3.csv("app/skater-mode/morley.csv", function render (error, csv) {
-    function render (csv) {
-        if (!csv || !csv.length) { return; }
-      var data = [];
-      console.log('csv', csv);
-      csv.forEach(function(x) {
-        var e = Math.floor(x.plot_number - 1),
-            r = Math.floor(x.index - 1),
-            s = Math.floor(x.value),
-            d = data[e];
-        if (!d) d = data[e] = [s];
-        else d.push(s);
-        if (s > max) max = s;
-        if (s < min) min = s;
-      });
+    function render (raw_data) {
+        if (!raw_data || !raw_data.length) { return; }
+        var data = [];
+        raw_data.forEach(function(x) {
+            var e = Math.floor(x.plot_number - 1),
+                r = Math.floor(x.index - 1),
+                s = Math.floor(x.value),
+                d = data[e];
+            
+            if (!d) d = data[e] = [s];
+            else d.push(s);
+            if (s > max) max = s;
+            if (s < min) min = s;
+        });
 
-      chart.domain([min, max]);
-      console.log('data', data);
-      var svg = d3.select("#box-and-whisker-container")
-        .selectAll("svg")
-          .data(data)
-        .enter().append("svg")
-          .attr("class", "box")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.bottom + margin.top)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-          .call(chart);
+        chart.domain([min, max]);
+        // console.log('data', data);
+        d3.select("#box-and-whisker-container svg").remove();
+        var svg = d3.select("#box-and-whisker-container")
+            .selectAll("svg")
+            .data(data)
+            .enter().append("svg")
+            .attr("class", "box")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.bottom + margin.top)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(chart);
 
-      setInterval(function() {
-        svg.datum(randomize).call(chart.duration(1000));
-      }, 2000);
     };
-
-    function randomize(d) {
-      if (!d.randomizer) d.randomizer = randomizer(d);
-      return d.map(d.randomizer);
-    }
-
-    function randomizer(d) {
-      var k = d3.max(d) * .02;
-      return function(d) {
-        return Math.max(min, Math.min(max, d + k * (Math.random() - .5)));
-      };
-    }
 
     // Returns a function to compute the interquartile range.
     function iqr(k) {
@@ -130,152 +111,7 @@ function SkaterModeLink (
       };
     }
 
-    // var left_margin_from_foo;
-    // scope.widerLeftColumn = false;
-    // scope.charting_data = [];
-    // scope.metrics = [];
-    // scope.render = render();
-    // scope.setMetricFromListClick = setMetricFromListClick;
-    
-    // angular.forEach( scope.$parent.metrics , function ( item ) {
-    //     scope.metrics.push(item.metric);
-    // });
-    
-    // scope.$on('skater_metrics', function (event, metrics) {
-    //     scope.metrics = metrics;
-    // });
-    
-    // scope.$watch('data', updateChartingData);
-    // scope.$watch('metric', updateChartingData);
-    
-    // function updateChartingData(newVal, oldVal) {
-    //     var data = scope.data;
-    //     if (!data.length) {return;}
-    
-    //     scope.charting_data.length = 0;    
-    //     scope.widerLeftColumn = false;
-    //     if ( scope.metrics.indexOf(newVal) > -1 ) {
-    //         setChartingData();
-    //     }
-
-    //     render(scope.charting_data);
-    //     return;
-
-    //     function setChartingData() {
-    //         data.map(function (entity) {    
-    //             var name; 
-    //             if ( entity.Player_Name ) {
-    //                 name = entity.Player_Name;
-    //                 scope.widerLeftColumn = true;
-    //             } else if ( entity.FullName ) {
-    //                 name = entity.FullName
-    //                 scope.widerLeftColumn = true;
-    //             } else {
-    //                 name = entity.teamname;
-    //             }
-
-    //             scope.charting_data.push({
-    //                 metric : newVal,
-    //                 value: entity[newVal], 
-    //                 entity : name
-    //             });
-    //         });
-    //     }
-    // }
-
-    // function render (render_data) {
-    //     // console.log('running render with render_data', render_data);
-    //     if (!render_data) return;
-    //     d3.selectAll('#bar-chart svg').remove();
-
-    //     var renderTimeout;
-    //     var margin = parseInt(attrs.margin) || 20,
-    //         barHeight = parseInt(attrs.barHeight) || 20,
-    //         barPadding = parseInt(attrs.barPadding) || 5; 
-        
-    //     var left_margin = 175,
-    //         my_font_size = 18,
-    //         value_text_margin = left_margin*0.7;
-
-    //     if (scope.widerLeftColumn) {
-    //         left_margin = 250;
-    //         my_font_size = 15;
-    //         value_text_margin = left_margin*0.85;
-    //     }
-        
-    //     var svg = d3.select('#bar-chart')
-    //         .append('svg').style('width', '100%');
-
-    //     render_data.sort(function(a, b) {
-    //         return parseFloat(b.value) - parseFloat(a.value);
-    //     });
-        
-    //     var data_length = render_data.length;
-    //     var chart_length = scope.chart_length;
-    //     if (chart_length && (chart_length < data_length)) {
-    //         render_data.splice(chart_length, data_length - chart_length);
-    //     }
-
-    //     if (renderTimeout) clearTimeout(renderTimeout);
-
-    //     renderTimeout = setTimeout(function() {
-    //         var container_width = d3.select('#bar-chart-container').node().getBoundingClientRect().width;
-    //         var width = container_width - left_margin*0.5,
-    //             height = render_data.length * (barHeight + barPadding),
-    //             color = d3.scale.category20b(),
-    //             min = d3.min(render_data, function(d) { return d.value; }),
-    //             max = d3.max(render_data, function(d) { return d.value; });
-            
-    //         var xScale = d3.scale.linear()
-    //                 .domain([min,max])
-    //                 .range([left_margin*0.5, width-left_margin]);
-
-    //         svg.attr('height', height);
-            
-    //         svg.selectAll('rect')
-    //         .data(render_data).enter()
-    //         .append('rect')
-    //         .attr('height', barHeight)
-    //         .attr('width', width/2)
-    //         .attr('x', left_margin + Math.round(margin / 2))
-    //         .attr('y', function(d, i) {
-    //             return i * (barHeight + barPadding);
-    //         })
-    //         .attr('fill', function(d) {
-    //             return color(d.value);
-    //         })
-    //         .transition().duration(1000)
-    //         .attr('width', function(d) {
-    //             return xScale(d.value);
-    //         });
-            
-    //         svg.selectAll('text.entity')
-    //         .data(render_data).enter()
-    //         .append('text')
-    //         .attr('fill', '#000').attr('y', function(d, i) {
-    //             return i * (barHeight + barPadding) + 15;
-    //         })
-    //         .attr('x', left_margin*0.05)
-    //         .text(function(d) {
-    //             return d.entity;
-    //         })
-    //         .style("font-size", my_font_size);
-
-    //         svg.selectAll('text.value')
-    //         .data(render_data).enter()
-    //         .append('text')
-    //         .attr('fill', '#000').attr('y', function(d, i) {
-    //             return i * (barHeight + barPadding) + 15;
-    //         })
-    //         .attr('x', value_text_margin).text(function(d) {
-    //             return parseFloat(d.value).toFixed(2);
-    //         })
-    //         .style("font-size", my_font_size*0.8);
-
-    //     }, 200);
-    // };
-
-    // function setMetricFromListClick ($event, met) { 
-    //     scope.metric = met;
-    // } 
+    function setMetricFromListClick ($event, met) { 
+        scope.metric = met;
+    } 
 }
--- app/skater-mode/skater-mode.services.js
+++ app/skater-mode/skater-mode.services.js
@@ -11,35 +11,45 @@ function skaterModeServices() {
         createRenderData: createRenderData,
     }
     // return array of objects for d3js chart
-    function createRenderData(payload, metric) {
-        var data = setChartingData(payload, metric);
+    function createRenderData(payload, metrics) {
+        var data = [];
+        var charting_data = [];
+        if ( angular.isArray(metrics) ) {
+            metrics.map(function function_name(metric, index) {
+                // console.log('metric', metric);
+                var plot_number = index + 1;
+                data = setChartingData(payload, metric, plot_number);       
+            });
+        } else {
+            var plot_number = 1;
+            data = setChartingData(payload, metrics, plot_number);
+        }
         // console.log(' new data createRenderData', data);
         return data;
-    }
-    function setChartingData(data, metric) {
-        var charting_data = [];
-        data.map(function (entity, index) {    
-            var name, team; 
-            if ( entity.Player_Name ) {
-                name = entity.Player_Name;
-                team = entity.Team;
-            } else if ( entity.FullName ) {
-                name = entity.FullName
-                team = entity.Team;
-            } else {
-                name = entity.teamname;
-            }
+        function setChartingData(data, metric, plot_number) {
+            data.map(function (entity, index) {    
+                var name, team; 
+                if ( entity.Player_Name ) {
+                    name = entity.Player_Name;
+                    team = entity.Team;
+                } else if ( entity.FullName ) {
+                    name = entity.FullName
+                    team = entity.Team;
+                } else {
+                    name = entity.teamname;
+                }
 
-            charting_data.push({
-                plot_number : "1",
-                metric : metric,
-                value: entity[metric], 
-                entity : name,
-                index : index + 1,
-                team : team
+                charting_data.push({
+                    plot_number : plot_number,
+                    metric : metric,
+                    value: entity[metric], 
+                    entity : name,
+                    index : index + 1,
+                    team : team
+                });
             });
-        });
-        return charting_data;
+            return charting_data;
+        }
     }
     // return array of quartiles 
     function calcQuartiles(payload, metric) {
